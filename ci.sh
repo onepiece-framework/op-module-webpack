4a27031bdf1f1a4b153619bf95c01b231e5511c2
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-11-26 11:56:15 +0900

    Fix: cicd | v2.2.0

--- ci.sh
+++ ci.sh
@@ -1,37 +1,62 @@
-<?php
-/** op-core:/ci.php
- *
- * @created    2023-01-01
- * @version    2.2.2
- * @package    op-core
- * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
- * @copyright  Tomoaki Nagahara All right reserved.
- */
 
- /** Declare strict
- *
- */
-declare(strict_types=1);
+ ## op-core:/ci.sh
+ #
+ # Call from git pre-push
+ #
+ # @created    2022-10-31
+ # @updated    2023-02-09 v2.0
+ # @updated    2023-02-14 v2.1
+ # @updated    2023-04-13 v2.2
+ # @version    2.2.0
+ # @package    op-core
+ # @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ # @copyright  Tomoaki Nagahara All right reserved.
 
-/** namespace
- *
- */
-namespace OP;
+# Get command
+COMMAND=$(ps -ocommand= -p $PPID)
 
-//	...
-$php = $_SERVER['_'];
+# Parse
+ARRAY=(${COMMAND//,/ })
+REMOTE=${ARRAY[2]}
+BRANCH=${ARRAY[3]}
+PHP=`php -r "echo PHP_MAJOR_VERSION.PHP_MINOR_VERSION;"`
 
-//	...
-$argv1 = $_SERVER['argv'][1] ?? '';
-$argv2 = $_SERVER['argv'][2] ?? '';
-$argv3 = $_SERVER['argv'][3] ?? '';
-$argv4 = $_SERVER['argv'][4] ?? '';
-$argv5 = $_SERVER['argv'][5] ?? '';
+# Get current branch name
+#BRANCH=`git rev-parse --abbrev-ref HEAD`
+if [ ! $BRANCH ]; then
+  echo "ci.sh: Empty branch name."
+  exit 1
+fi
 
-/* @var $output array   */
-/* @var $status integer */
-exec("{$php} ../ci.php {$argv1} {$argv2} {$argv3} {$argv4} {$argv5}", $output, $status);
-if( $output ){
-	echo join("\n", $output)."\n";
-}
-exit($status);
+# Check if branch name
+if [[  $BRANCH =~ ^php([0-9]{2})$ ]]; then
+    PHP=${BASH_REMATCH[1]}
+else
+    PHP=`php -r "echo PHP_MAJOR_VERSION.PHP_MINOR_VERSION;"`
+fi
+
+# Set CI saved commit id file name
+CI_FILE=".ci_commit_id_"$BRANCH"_php"$PHP
+#echo $CI_FILE
+
+# Check if file exists
+if [ ! -f $CI_FILE ]; then
+  echo "ci.sh: Does not file exists. ($CI_FILE)"
+  exit 1
+fi
+
+# Get commit id
+CI_COMMIT_ID=`cat $CI_FILE`
+#echo $CI_COMMIT_ID
+
+# Get correct commit id
+COMMIT_ID=`git rev-parse $BRANCH`
+#echo $COMMIT_ID
+
+#
+if [ $COMMIT_ID != $CI_COMMIT_ID ]; then
+  echo "ci.sh: Unmatch commit id"
+  echo $COMMIT_ID branch=$BRANCH
+  echo $CI_COMMIT_ID $CI_FILE
+  exit 1
+fi
