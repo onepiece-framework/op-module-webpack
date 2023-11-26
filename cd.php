02c5d7052ef8662292ea3dfbb02fe0264ca598e8
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-11-26 11:43:06 +0900

    New: cicd

---
+++ cd.php
@@ -0,0 +1,34 @@
+<?php
+/** op-core:/cd.php
+ *
+ * @created    2023-02-10
+ * @version    2.1.2
+ * @package    op-core
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+ /** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+$argv1 = $_SERVER['argv'][1] ?? '';
+$argv2 = $_SERVER['argv'][2] ?? '';
+$argv3 = $_SERVER['argv'][3] ?? '';
+$argv4 = $_SERVER['argv'][4] ?? '';
+$argv5 = $_SERVER['argv'][5] ?? '';
+
+/* @var $output array   */
+/* @var $status integer */
+exec("php ../cd.php {$argv1} {$argv2} {$argv3} {$argv4} {$argv5}", $output, $status);
+if( $output ){
+	echo join("\n", $output)."\n";
+}
+exit($status);
