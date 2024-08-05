dac0ba7eee430b1f3bf5f21e4a12a0ffb3007a98
TomoakiMacBookAirM1 <TomoakiNagahara@github.com>
2024-08-06 04:12:09 +0900

    2024: sample > sample.js, sample.css

---
+++ sample/sample.js
@@ -0,0 +1,11 @@
+
+/** op-module-webpack:/sample/sample.css
+ * 
+ * @created    2024-08-05
+ * @version    1.0
+ * @package    op-module-webpack
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+console.log('webpack:/sample/sample.js is loaded');
