5a1a4d0467dfef7ad9d3b7e1419afe261a7c7b0c
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-04-16 19:14:31 +0900

    Add: content > index.php | PHPDoc

---
+++ content/index.php
@@ -0,0 +1,14 @@
+<?php
+/**	op-module-webpack:/content/index.php
+ *
+ * @created    2025-04-16
+ * @version    1.0
+ * @package    op-module-webpack
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
